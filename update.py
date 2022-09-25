1c7a0f135f2b3d74ff324759030ee4f1c3538ecc
FakeCore <flowunlight@gmail.com>
2022-09-25 16:49:31 +0800

    update update.py

--- update.py
+++ update.py
@@ -15,15 +15,12 @@ if __name__ == "__main__":
         modTime = time.localtime(os.stat(base+i).st_mtime)
         modified_time = time.strftime("%Y-%m-%d %H:%M:%S +0800", modTime)
         file = base+i
-        print(modified_time,file)
         fn = open(file,'r')
         lines = fn.readlines()
         for i in range(0,len(lines)):
             line = lines[i]
             if line.find("last_modified_at") != -1:
-                print(line,i)
                 lines[i] ="late_modified_at: " + modified_time + "\n"
-                print(lines[i])
                 fn.close()
                 fn=open(file,"w+")
                 fn.writelines(lines)
